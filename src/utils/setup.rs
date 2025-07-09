760b1a10c695ef57c31fffa4a572cfce957c0aa8
Louis Kauer <laky.dev@proton.me>
2025-07-09 14:25:33 +0200

    feat(setup): implement configuration setup for applications and tenants

--- src/utils/setup.rs
+++ src/utils/setup.rs
@@ -1,8 +1,11 @@
 use crate::models::services_config::ServicesConfig;
 use crate::routes::routes::setup_routes;
 use crate::services::authorize_code_service::AuthorizeCodeService;
+use crate::services::config::application_service::ApplicationService;
+use crate::services::config::tenant_service::TenantService;
 use crate::services::session_service::SessionService;
 use crate::services::user_service::UserService;
+use crate::utils::config_loader::{load_applications_config, load_tenants_config};
 use crate::utils::database::create_postgres_pool;
 use crate::utils::redis_utils::create_redis_pool;
 use axum::Router;
@@ -17,6 +20,10 @@ pub async fn setup_server() -> Result<(), anyhow::Error> {
         .expect("Failed to setup database and Redis pools");
 
     let services = setup_services(sqlx_pool, redis_pool);
+
+    setup_configurations(services.clone())
+        .await
+        .expect("Failed to load configurations");
 
     let (listener, main_router) = setup_router(services)
         .await
@@ -50,11 +57,15 @@ fn setup_services(
     let user_service = UserService::new(sqlx_pool.clone());
     let auth_code_service = AuthorizeCodeService::new(sqlx_pool.clone(), redis_pool.clone());
     let session_service = SessionService::new(sqlx_pool.clone(), redis_pool);
+    let tenant_service = TenantService::new(sqlx_pool.clone());
+    let application_service = ApplicationService::new(sqlx_pool);
 
     Arc::new(ServicesConfig {
         user_service,
         auth_code_service,
         session_service,
+        tenant_service,
+        application_service,
     })
 }
 
@@ -68,4 +79,32 @@ async fn setup_router(
     let listener = TcpListener::bind(&address).await?;
 
     Ok((listener, main_router))
+}
+
+async fn setup_configurations(services: Arc<ServicesConfig>) -> Result<(), anyhow::Error> {
+    let tenants_config = load_tenants_config("config/tenants.yaml").await?;
+    let applications_config = load_applications_config("config/applications.yaml").await?;
+
+    for tenant in tenants_config.tenants {
+        if let Err(_) = services
+            .tenant_service
+            .create_tenant(Some(tenant.id), &tenant.name)
+            .await
+        {
+            println!("Tenant {} already exists. Skipping...", tenant.id);
+        }
+    }
+
+    for application in applications_config.applications {
+        let application_id = application.id.clone();
+        if let Err(_) = services
+            .application_service
+            .create_application(application)
+            .await
+        {
+            println!("Application {} already exists. Skipping...", application_id);
+        }
+    }
+
+    Ok(())
 }
