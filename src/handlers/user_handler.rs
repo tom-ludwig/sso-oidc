f40a02ab555d3ce140843172d8c2ce7796ed5eeb
Kuecken <JULIKUECKEN@GMAIL.COM>
2025-07-09 09:29:38 +0200

    resolve last merge-conflict

--- src/handlers/user_handler.rs
+++ src/handlers/user_handler.rs
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-use std::sync::Arc;
-
-use axum::{Extension, http::StatusCode, response::IntoResponse};
-
-use crate::models::services_config::ServicesConfig;
-
-pub async fn get_test_user(
-    Extension(services): Extension<Arc<ServicesConfig>>,
-) -> impl IntoResponse {
-    services.user_service.test_create_user().await;
-
-    StatusCode::OK
-}
-=======
 // pub async fn get_test_user(
 //     Query(params): Query<AuthorizeRequest>,
 //     TypedHeader(cookies): TypedHeader<Cookie>,
@@ -22,4 +7,3 @@ pub async fn get_test_user(
 //
 //     return StatusCode::OK;
 // }
->>>>>>> master
